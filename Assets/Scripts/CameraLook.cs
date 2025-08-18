e35c89e79a474f3315e2a932ca9a8c19809344a2
johnklima <klima.de.sa@gmail.com>
2025-08-18 14:42:19 +0200

    hello

--- Assets/Scripts/CameraLook.cs
+++ Assets/Scripts/CameraLook.cs
@@ -5,12 +5,12 @@ public class CameraLook : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("Hello World");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("Hello Update");
     }
 }
