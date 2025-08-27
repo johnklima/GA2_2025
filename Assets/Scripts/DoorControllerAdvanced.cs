0bce0a47c6a67dbef8b71e7308e6f2f8a3513936
johnklima <klima.de.sa@gmail.com>
2025-08-27 17:18:56 +0200

    a bit more cleanup

--- Assets/Scripts/DoorControllerAdvanced.cs
+++ Assets/Scripts/DoorControllerAdvanced.cs
@@ -1,25 +1,41 @@
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class DoorControllerAdvanced : MonoBehaviour
 {
 
-    public Animator doorAnim;
+    private Animator doorAnim;
+
+    //for the polling aproach (see method aproach below)
     public bool openthedoor = false;
     public bool closethedoor = false;
+
+    private VisualEffect trails;
+    private AudioSource sound;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         doorAnim = transform.GetComponent<Animator>();
+        trails = transform.GetComponent<VisualEffect>();
+        sound = transform.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+        //example of using polling for bools
         if(openthedoor)
         {
-            doorAnim.SetTrigger("OpenDoor");
+
             //tell animator to open door
+            doorAnim.SetTrigger("OpenDoor");
+            //vfx and sound are played the same!
+            trails.Play();
+            sound.Play();
+
+            
             openthedoor = false;
         }
 
@@ -27,6 +43,8 @@ public class DoorControllerAdvanced : MonoBehaviour
         {
             //tell animator to close door
             doorAnim.SetTrigger("CloseDoor");
+            trails.Stop();
+
             closethedoor = false;
         }
     }
